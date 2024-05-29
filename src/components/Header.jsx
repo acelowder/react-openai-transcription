7be771f7c5f027dda55f3d730d22618c9a508712
Ace <ace.lowder@gmail.com>
2024-05-28 21:35:44 -0700

    Add header functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,19 @@ import { FaPlus } from "react-icons/fa";
 export function Header() {
   return (
     <header>
-      <h2>
-        Sound<span className="primary">Scribe</span>
-      </h2>
-      <button>
-        <h3>New</h3>
-        <h2 className="icon">
-          <FaPlus />
+      <a href="/">
+        <h2>
+          Sound<span className="primary">Scribe</span>
         </h2>
-      </button>
+      </a>
+      <a href="/">
+        <button>
+          <h3>New</h3>
+          <h2 className="icon">
+            <FaPlus />
+          </h2>
+        </button>
+      </a>
     </header>
   );
 }
